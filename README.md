2505da2ad43e4ad597fb9826b79e42d0768e87c8
Akshansh <akshanshz@gmail.com>
2025-08-02 16:25:02 +0200

    requirements added

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # visa
 ML deployment with mongoDB
+
+Install:
+step 1.
+conda install numpy pandas scipy matplotlib seaborn scikit-learn xgboost catboost
+
+Step 2. pip install -r requirements.txt
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="1.0.0",
+    author="ak",
+    author_email="akshanshz@gmail.com",
+    packages=find_packages()
+)
