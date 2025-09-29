370a3dc3c0558bacf2b82db2bc94c996441f89c5
Akshansh <akshanshz@gmail.com>
2025-09-29 11:10:11 +0200

    data ingestion - constants

---
+++ us_visa/constants/__init__.py
@@ -0,0 +1,18 @@
+import os
+from datetime import date
+
+DATABASE_NAME = "DB_visa"
+
+COLLECTION_NAME = "collection_visa"
+
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = "usvisa"
+ARTIFACT_DIR: str = "artifact"
+
+
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+
+FILE_NAME: str = "usvisa.csv"
+MODEL_FILE_NAME = "model.pkl"
