237be195773dc4a241d67ea5d3b41abb333fa321
Akshansh <akshanshz@gmail.com>
2025-10-10 20:31:14 +0200

    data ingestion mongodb connection

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -1,11 +1,14 @@
 import os
 from datetime import date
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_NAME = "DB_visa"
 
 COLLECTION_NAME = "collection_visa"
 
-MONGODB_URL_KEY = "MONGODB_URL"
+MONGODB_URL_KEY = os.getenv("MONGODB_URL")
 
 PIPELINE_NAME: str = "usvisa"
 ARTIFACT_DIR: str = "artifact"
@@ -16,3 +19,12 @@ TEST_FILE_NAME: str = "test.csv"
 
 FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "visa_data"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
